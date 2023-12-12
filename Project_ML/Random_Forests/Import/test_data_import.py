73f4d9889ae6ca50cb20db6aa53d88e22d48a0db
Patrick Fuchs <p.fuchs@hm.edu>
2023-12-12 22:35:09 +0100

    --

--- Project_ML/Random_Forests/Import/test_data_import.py
+++ Project_ML/Random_Forests/Import/test_data_import.py
@@ -6,18 +6,19 @@ import os
 
 class TestDatasetImport(unittest.TestCase):
     def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
         self.temp_files = []
         for i in range(4):
-            temp, path = tempfile.mkstemp(suffix='.csv')
-            os.write(temp, f"A,B,C,D\n1,2,3,{i+1}\n5,6,7,{i+1}\n9,10,11,{i+1}\n".encode())
-            os.close(temp)
-            self.temp_files.append(path)
+            file_path = os.path.join(self.temp_dir.name, f"temp_data_{i}.csv")
+            with open(file_path, 'w') as f:
+                f.write(f"A,B,C,D\n1,2,3,{i+1}\n5,6,7,{i+1}\n9,10,11,{i+1}\n")
+            self.temp_files.append(file_path)
             if i == 0:
-                self.data_import = DatasetImport(path[:-4])
+                # Erstellt die DatasetImport-Instanz basierend auf dem Dateinamen ohne die ".csv" Erweiterung.
+                self.data_import = DatasetImport(file_path[:-4])
 
     def tearDown(self):
-        for f in self.temp_files:
-            os.remove(f)
+        self.temp_dir.cleanup()  # Entfernt das Verzeichnis und alle darin enthaltenen Dateien.
 
     def test_import_data(self):
         self.assertEqual(self.data_import.dataframe.shape, (3, 4))
